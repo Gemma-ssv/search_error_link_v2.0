0f8f3f399015c1f7f428311388d54db9fb4f3659
Gemma-ssv <eldarbolshov@gmail.com>
2024-11-28 15:18:48 +0300

    create print_choice

--- utils.py
+++ utils.py
@@ -58,3 +58,49 @@ def is_valid_url(url_input):
         bool: Правильный формат или неправильный формат ссылки.
     """
     pass
+
+def print_choice(urls: set) -> set:
+    """
+    Выводит информацию о программе. Запрашивает у пользователя ссылки.
+
+    Аргументы:
+        urls (set): Получает пустой список, в который будут записаны ссылки.
+
+    Возвращает:
+        set: Возвращает список ссылок, которые ввёл пользователь.
+    """
+    example_text = (
+        "Программа предназначена для проверки ссылок в новостях в интернет-магазине.\n"
+        "Введите ссылку в формате - https://домен/путь/\n"
+        "Например - https://gemma.by/news/\n"
+        "После ввода нажмите - Enter.\n"
+    )
+    print_slowly(example_text)
+    
+    while True:
+        example_text = "Введите ссылку: "
+        print_slowly(example_text)
+        url_input = input().strip()
+
+        if is_valid_url(url_input):
+            urls.add(url_input)
+            while True:
+                choice_text = ("Выполнить поиск? Введите `да` или `нет`.\n"
+                               "Если `нет`, то можно будет добавить еще ссылку.\n")
+                print_slowly(choice_text)
+                next_input = input().lower().strip()
+
+                if next_input == 'да':
+                    answer_yes_text = "Дождитесь окончания выполнения программы.\n"
+                    print_slowly(answer_yes_text)
+                    return urls
+                elif next_input == 'нет':
+                    break
+                else:
+                    wrong_answer_text = "Неправильный ответ. Требуется ввести `да` или `нет`.\n"
+                    print_slowly(wrong_answer_text)
+                    continue
+        else:
+            wrong_link_text = "Вы ввели неправильный формат ссылки. Попробуйте еще раз.\n"
+            print_slowly(wrong_link_text)
+            continue
